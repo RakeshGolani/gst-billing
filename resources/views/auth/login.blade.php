37ed682c4bea0adfde3d7ff3526f9ce63cb40f8d
Rakesh Golani <rakeshgolanieww@gmail.com>
2024-02-20 13:52:12 +0530

    add url in register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -62,6 +62,11 @@
                                     {{ __('Forgot Your Password?') }}
                                 </a>
                                 @endif
+                                
+                                <a class="btn btn-link" href="{{ route('register') }}">
+                                    {{ __('Register Your Account?') }}
+                                </a>
+                              
                             </div>
                         </div>
                     </form>
